72862261d8ae1d79d2a730e1d96be4a40bfd6069
Martin Kulicek <mkulicek@trask.cz>
2023-08-07 22:37:33 +0200

    Vytvoření Logout fce + úpravy is Open

--- src/main/java/webtest/page/app/MainPage.kt
+++ src/main/java/webtest/page/app/MainPage.kt
@@ -1,10 +1,8 @@
 package webtest.page.app
 
 import org.openqa.selenium.By
-import org.testng.Assert.fail
 import webtest.base.ComponentType
 import webtest.base.ElementDef
-import webtest.base.Info.Companion.of
 import webtest.page.common.AbstractTechnicalPage
 
 class MainPage : AbstractTechnicalPage() {
